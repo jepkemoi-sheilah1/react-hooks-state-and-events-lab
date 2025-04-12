5a90421bfc8726304bd887e125ee266a163f1c2d
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-04-12 06:57:24 +0300

    created item,shopping list and app components to complete lab ,fix item component topass test

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React , { useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // state variable called selectedCategory
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  // function to handle the filter change
+  const handleFilterChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  // filter the items based on the selected category
+  const filteredItems = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
